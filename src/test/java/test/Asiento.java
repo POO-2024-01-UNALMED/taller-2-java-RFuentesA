04550d6f11865637178d79fb302bdeca1e906ba8
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-02-20 10:58:38 -0500

    Create Asiento.java
    Clase Asiento con su metodo

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,16 @@
+package test;
+import java.util.Arrays;
+
+public class Asiento {
+  public String color;
+  public int precio;
+  public int registro;
+
+  public void cambiarColor(String color){
+
+    String[] colores = {"blanco","verde","rojo","negro","amarillo"}
+    if (Arrays.asList(colores).contains(color)){
+      this.color = color;
+    }
+  }
+}
